9c5bfbe429abc7e8f5fb3602f4bef8f2e830bdd8
Ribble <kannenbergkalle@gmail.com>
2025-11-27 17:57:56 +0100

    Add files via upload

---
+++ Bombed.js
@@ -0,0 +1,27 @@
+elements.big_boom = {
+    color: "#ffaa00",
+    category: "Bombed",
+    state: "solid",
+    density: 9999,
+    behavior: behaviors.WALL,
+
+    onPlace(pixel) {
+        pixel.startTime = pixelTicks; // Timer starten
+    },
+
+    tick(pixel) {
+        if (pixelTicks - pixel.startTime >= 2) {
+            pixel.startTime = pixelTicks;
+
+            let below = pixelMap[pixel.x]?.[pixel.y + 1];
+
+            if (!below) {
+                tryMove(pixel, pixel.x, pixel.y + 1);
+            }
+            else {
+                explodeAt(pixel.x, pixel.y, 1000);
+                deletePixel(pixel.x, pixel.y);
+            }
+        }
+    }
+};
